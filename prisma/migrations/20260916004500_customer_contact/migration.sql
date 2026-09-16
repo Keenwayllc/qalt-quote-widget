3184284d21ac3ba6655b52f64b8de2cb8466b10c
Keenwayllc <info@gokeenway.com>
2026-09-16 13:15:52 -0700

    Add customer-facing contact settings

---
+++ prisma/migrations/20260916004500_customer_contact/migration.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "Company" ADD COLUMN IF NOT EXISTS "customerContactDepartment" TEXT;
+ALTER TABLE "Company" ADD COLUMN IF NOT EXISTS "customerContactEmail" TEXT;
+ALTER TABLE "Company" ADD COLUMN IF NOT EXISTS "customerContactPhone" TEXT;
+ALTER TABLE "Company" ADD COLUMN IF NOT EXISTS "customerContactHours" TEXT;
