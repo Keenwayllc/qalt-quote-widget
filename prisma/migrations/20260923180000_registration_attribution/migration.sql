6e6da273e6fca01495a001cb69091f43ef668798
Keenwayllc <info@gokeenway.com>
2026-09-23 11:07:15 -0700

    Add registration attribution migration

---
+++ prisma/migrations/20260923180000_registration_attribution/migration.sql
@@ -0,0 +1,9 @@
+ALTER TABLE "Company"
+ADD COLUMN "registrationSource" TEXT,
+ADD COLUMN "registrationReferrer" TEXT,
+ADD COLUMN "registrationLandingPage" TEXT,
+ADD COLUMN "registrationUtmSource" TEXT,
+ADD COLUMN "registrationUtmMedium" TEXT,
+ADD COLUMN "registrationUtmCampaign" TEXT,
+ADD COLUMN "registrationUtmTerm" TEXT,
+ADD COLUMN "registrationUtmContent" TEXT;
